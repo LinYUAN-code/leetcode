390ad8d600cf375359b9bf29612c73738a375f60
LinYUAN-code <1138842605@qq.com>
2021-09-26 09:25:26 +0800

    2021.9.26

---
+++ leetcode/9.go
@@ -0,0 +1,17 @@
+package leetcode
+
+/*
+   1.转化位字符串然后判断
+   2.在数字的基础上进行判断
+*/
+func isPalindrome(x int) bool {
+	if x < 0 ||  (x%10==0 && x!=0) {
+		return false
+	}
+	tem := 0
+	for x>tem {
+		tem = tem*10 + x%10
+		x /= 10
+	}
+	return x==tem || x==tem/10
+}
