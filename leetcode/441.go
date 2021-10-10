481e5e00a8a422ea47ca2bb25e9d6df877e15e3c
LinYUAN-code <1138842605@qq.com>
2021-10-10 13:09:44 +0800

    2021.10.10

---
+++ leetcode/441.go
@@ -0,0 +1,16 @@
+package leetcode
+
+
+func arrangeCoins(n int) int {
+	// 二分查找
+	l,r := int64(0),int64(n)
+	for l<r {
+		var mid = (l + r + 1)/2
+		if mid*(mid+1)/2 > int64(n) {
+			r = mid - 1
+		} else {
+			l = mid
+		}
+	}
+	return int(l)
+}
