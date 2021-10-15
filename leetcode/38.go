5579d939ce63edea392dcd4b23ea2dbac901782c
LinYUAN-code <1138842605@qq.com>
2021-10-15 09:54:38 +0800

    2021.10.15

---
+++ leetcode/38.go
@@ -0,0 +1,24 @@
+package leetcode
+
+import "strconv"
+
+func countAndSay(n int) string {
+	// 可以通过递归寻找
+	// 暴力从 n ---> n+1 需要的时间复杂度是O(长度) -》时间复杂度n^2
+	ans := "1"
+	for i := 2; i <= n; i++ {
+		s := ""
+		// 寻找一个前缀x
+		pre := len(ans) - 1
+		for pre >= 0 {
+			end := pre
+			for end-1 >= 0 && ans[end] == ans[end-1] {
+				end--
+			}
+			s = strconv.Itoa(pre-end+1) + string(ans[pre]) + s
+			pre = end - 1
+		}
+		ans = s
+	}
+	return ans
+}
