bce37441535f53b13e76b818b2ad8f0cdee4af15
LinYUAN-code <1138842605@qq.com>
2021-10-22 09:18:14 +0800

    2021.10.22

---
+++ leetcode/2021.10.17/169.go
@@ -0,0 +1,20 @@
+package leetcode
+
+func majorityElement(nums []int) int {
+	// 数组总是存在多数元素
+	n := 0
+	num := 0x3f3f3f3f
+	for i := 0; i < len(nums); i++ {
+		if n == 0 {
+			num = nums[i]
+			n = 1
+			continue
+		}
+		if nums[i] == num {
+			n++
+		} else {
+			n--
+		}
+	}
+	return num
+}
