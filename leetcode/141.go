cd25fe1698fb7114a79a492403a62cf913f537a2
LinYUAN-code <1138842605@qq.com>
2021-10-27 09:43:06 +0800

    2021.10.27

---
+++ leetcode/141.go
@@ -0,0 +1,55 @@
+package leetcode
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+ func hasCycle(head *ListNode) bool {
+    //直接原地修改ListNode---来判断
+    if head == nil {
+        return false
+    }
+    for {
+        if head.Next == nil {
+            return false
+        }
+        if head.Val == 100005 {
+            return true
+        }
+        head.Val = 100005
+        head = head.Next
+    }
+    return false
+}
+
+
+
+
+// 快慢指针
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+//  func hasCycle(head *ListNode) bool {
+//     if head == nil {
+//         return false
+//     }
+//     slow,fast := head,head
+//     for {
+//         if slow==nil || fast==nil ||fast.Next==nil {
+//             return false
+//         }
+//         slow = slow.Next
+//         fast = fast.Next.Next
+//         if slow == fast {
+//             return true
+//         }
+//     }
+//     return false
+// }
