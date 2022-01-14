36c306125afe870f4f4b0796161ed1146dd19110
林任骏 <1138842605@qq.com>
2022-01-14 13:47:37 +0800

    2022.1.14

---
+++ leetcode/373.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @param {number} k
+ * @return {number[][]}
+ */
+ var kSmallestPairs = function(nums1, nums2, k) {
+    //  很经典的一道题目
+    const q = new MinPriorityQueue({
+        priority: (v) => {
+            return nums1[v[0]] + nums2[v[1]];
+        }
+    });
+    for(let i=0;i<nums1.length;i++) {
+        q.enqueue([i,0]);
+    }
+    let ans = [];
+    while(k && q.size()) {
+        let top = q.dequeue().element;
+        k--;
+        ans.push([nums1[top[0]],nums2[top[1]]]);
+        if(top[1]+1<nums2.length) {
+            q.enqueue([top[0],top[1]+1]);
+        }
+    }
+    return ans;
+};
